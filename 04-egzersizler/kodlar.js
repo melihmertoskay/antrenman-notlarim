f3cae8eaa44ab83a3982604d3ea532082a128489
Melih Mert Oskay <melihmertoskay@gmail.com>
2026-09-17 19:29:48 +0300

    Dambıl sumo squat egzersiz kodunu ekle

--- 04-egzersizler/kodlar.js
+++ 04-egzersizler/kodlar.js
@@ -2,7 +2,7 @@ const EGZERSIZ_KODLARI={
 "Omuz ve kol ısınması":"I1","Omuz ve sırt ısınması":"I2","Squat ve bacak ısınması":"I3","Genel ısınma":"I4","Sırt ve omuz ısınması":"I5",
 "Dambıl bench press":"G1","Incline dambıl press":"G2","Şınav":"G3","Dar tutuş şınav":"G4","Dambıl shoulder press":"G5","Dambıl lateral raise":"G6","Pike şınavı":"G7","Dambıl overhead triceps extension":"G8",
 "Tek kol dambıl row":"S1","Dambıl reverse fly":"S2","Dambıl pullover":"S3","Dambıl biceps curl":"S4","Dambıl hammer curl":"S5","Bant row":"S6","Tek kol bant row":"S7","Bant pull-apart":"S8","Bant ile lat pullover":"S9","Superman":"S10","Bant ile biceps curl":"S11",
-"Dambıl calf raise":"B1","Goblet squat":"B2","Deadlift":"B3","Dambıl hip thrust":"B4","Bulgarian split squat":"B5","Dambıl Romanian deadlift":"B6","Yavaş squat":"B7","Tek bacak glute bridge":"B8","Reverse lunge":"B9","Tek bacak calf raise":"B10","Glute bridge":"B11","Calf raise":"B12","Ağırlıksız squat":"B13",
+"Dambıl calf raise":"B1","Goblet squat":"B2","Deadlift":"B3","Dambıl hip thrust":"B4","Bulgarian split squat":"B5","Dambıl Romanian deadlift":"B6","Yavaş squat":"B7","Tek bacak glute bridge":"B8","Reverse lunge":"B9","Tek bacak calf raise":"B10","Glute bridge":"B11","Calf raise":"B12","Ağırlıksız squat":"B13","Dambıl sumo squat":"B14",
 "Plank shoulder tap":"K1","Side plank":"K2","Plank":"K3",
 "Rahat yüzme":"Y1","Uzun kulaç ve kontrollü nefes":"Y2","Rahat ve kesintisiz yüzme":"Y3","Çok rahat yüzme":"Y4","Tempolu yüzme":"Y5","Bir rahat, bir orta tempo":"Y6","Rahat-orta tempoda kesintisiz yüzme":"Y7","Her 25 metrenin ikinci yarısında hızlan":"Y8"
 };
